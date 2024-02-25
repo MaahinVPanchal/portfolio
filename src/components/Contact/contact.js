ba7c54499c64263d61a4f50683d26302bd152a70
MaahinVPanchal <maahinpanchal@gmail.com>
2024-02-25 14:50:41 +0530

    emailjs-com updated

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -8,7 +8,7 @@ import FacebookIcon from "../../assets/facebook-icon.png";
 import TwitterIcon from "../../assets/twitter.png";
 import YoutubeIcon from "../../assets/youtube.png";
 import InstagramIcon from "../../assets/instagram.png";
-import emailjs from "@emailjs/browser";
+import emailjs from "@emailjs-com";
 
 const Contact = () => {
   const form = useRef();
