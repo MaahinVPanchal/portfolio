2526bc6dc086cdc0843e45dd4c71fa7c97b103e6
MaahinVPanchal <maahinpanchal@gmail.com>
2024-02-26 17:41:59 +0530

    add some text and done styling

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./work.css";
 
-import Portfolio7 from "../../assets/portfolio-2.png";
-import Portfolio8 from "../../assets/portfolio-3.png";
-import Portfolio9 from "../../assets/portfolio-4.png";
-import Portfolio10 from "../../assets/portfolio-5.png";
+import Portfolio7 from "../../assets/portfolio-7.png";
+import Portfolio8 from "../../assets/portfolio-8.png";
+import Portfolio9 from "../../assets/portfolio-9.png";
+import Portfolio10 from "../../assets/portfolio-10.png";
 
 const Works = () => {
   return (
     <section id="works">
-      <h2 className="worksTitle">What Next After Mern.</h2>
+      <h2 className="worksTitle">Blockchain As Begineer.</h2>
       <span className="worksDesc">
         I am eagerly awaiting to learn and pursue a career in blockchain
-        technology.
+        technology.I am starting with this part.
       </span>
       <div className="worksImgs">
         <img src={Portfolio7} alt="Portfolio7" className="worksImg" />
